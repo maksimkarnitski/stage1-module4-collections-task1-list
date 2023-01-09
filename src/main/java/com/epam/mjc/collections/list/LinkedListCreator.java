0b196a8ddffe0985ff58d50837c6d2e344d053b4
Maksim_Karnitski <maksim.kornitsky@gmail.com>
2023-01-09 12:28:54 +0100

    Implement

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> result = new LinkedList<>();
+        for (Integer i: sourceList) {
+            if (i % 2  != 0) {
+                result.add(0, i);
+            } else {
+                result.add(result.size(), i);
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,34 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
+
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        if (function(a) > function(b)) {
+            return 1;
+        } else if (function(a) < function(b)) {
+            return -1;
+        } else if (function(a) == function(b)) {
+            if (Integer.valueOf(a) > Integer.valueOf(b)) {
+                return 1;
+            } else {
+                return -1;
+            }
+        } else {
+            return 0;
+        }
+
     }
+
+    public int function(String a) {
+        int ai = Integer.parseInt(a);
+        return (int) Math.pow(5 * ai, 2) + 3;
+    }
+
+
 }
